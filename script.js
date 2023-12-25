5f4493b1f6e69f520d4da5a12ce49dde6bbce4d0
Askhat Tassybayev <a.tassybayev@gmail.com>
2023-12-25 16:53:09 +0600

    применил функцию isNUmber из прошлого урока и похоже она еще и приводит к числу, не сразу понял что confirm сам посебе возращает boolean

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Функция проверки на число и похоже приводит к числу тоже
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num) && num !== 0
+ }
+
 const startGame = function(){
     const guessNum = 10;
     let attempts = 10;
@@ -16,12 +21,17 @@ const startGame = function(){
              
         } else {
              
-            const number = +prompt("Угадай число от 1 до 100")
+            const number = prompt("Угадай число от 1 до 100")
+            
             if(number === null){
+                
                 alert("Игра окончена")
                 return;
-            }   
-                if(typeof number === 'number' && number !== 0){
+               
+            }  
+           
+                if(isNumber(number)){
+                    
                     if(number > guessNum){
                         alert("Загаданное число меньше, осталось попыток:" + attempts)
                         game()
@@ -29,12 +39,16 @@ const startGame = function(){
                     if(number < guessNum){
                         alert("Загаданное число больше, осталось попыток:" + attempts)
                         game()
+                    } else {
+                        if(confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")){
+                            
+                            startGame()
+                        } else {
+                            alert("Игра окончена")
+                            return
+                        }
+    
                     }
-                    if(number === guessNum){
-                        confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")
-                        startGame()
-                    }
-
                 } else {
                     alert("Введи число!");
                     game()
